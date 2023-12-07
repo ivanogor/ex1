636f3707f62619e06361833ed23bd9de64f7e366
ivanogor <ivanogor23@gmail.com>
2023-12-07 23:30:21 +0800

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        
+        System.out.println(task("101"));
+    }
+
+    public static boolean task(String word){
+        word = word.toLowerCase();
+        for (int i = 0; i < word.length() / 2; i++){
+            if (word.charAt(i) != word.charAt(word.length() - 1 - i)){
+                return false;
+            }
+        }
+        return true;
     }
 }
+
